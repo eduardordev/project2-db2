4b369051ec97a0b4988abe7c362c9187cc97c6f4
Alejandro Ortega <ort18248@uvg.edu.gt>
2024-04-18 17:43:50 -0600

    Update Aggregation01.js

--- front/src/pages/Aggregation01/Aggregation01.js
+++ front/src/pages/Aggregation01/Aggregation01.js
@@ -1,61 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import DataTable from '../../components/DataTable';
-import { Typography } from '@mui/material';
+import { Typography, Paper } from '@mui/material';
 
 function Aggregation01() {
-    const [data, setData] = useState([]);
-    const [selectedYear, setSelectedYear] = useState('2023'); // Default selected year
+    const [data, setData] = useState(0);
+    const [selectedStatus, setSelectedStatus] = useState('OS');
 
     useEffect(() => {
-        fetchData(selectedYear);
-    }, [selectedYear]);
+        fetchData(selectedStatus);
+    }, [selectedStatus]);
 
-    const fetchData = async (year) => {
+    const fetchData = async (status) => {
         try {
-            const response = await axios.get(`http://localhost:8000/invoices/monthly_sales_of_year/?year=${year}`);
-            const formattedData = response.data.map(item => ({
-                ...item,
-                sales: parseFloat(item.sales)
-            }));
-            setData(formattedData);
+            const response = await axios.get(`http://3.145.98.75:1913/api/inventories/totalQuantity/${status}`);
+            setData(response.data);
         } catch (error) {
             console.error('Error fetching data:', error);
         }
-    };
+    };  
 
-    const handleYearChange = (event) => {
-        setSelectedYear(event.target.value);
+    const handleStatusChange = (event) => {
+        setSelectedStatus(event.target.value);
     };
 
     return (
         <div>
-            <Typography variant="h4" gutterBottom>
-                Ventas Mensuales por Año
-            </Typography>
-            <div>
-                <label htmlFor="year">Seleccionar Año:</label>
-                <select id="year" value={selectedYear} onChange={handleYearChange} style={{ fontSize: '16px', marginLeft: '10px', padding: '2px' }}>
-                    <option value="2022">2022</option>
-                    <option value="2023">2023</option>
-                    <option value="2024">2024</option>
-                    {/* Add more years as needed */}
-                </select>
-            </div>
-            {data.length > 0 ? (
-                <DataTable
-                    table={{
-                        columns: [
-                            { Header: "año", accessor: "year", width: "25%" },
-                            { Header: "mes", accessor: "month", width: "30%" },
-                            { Header: "ventas", accessor: "sales" },
-                        ],
-                        rows: data
-                    }}
-                />
-            ) : (
-                <Typography variant="body1">No hay datos disponibles para el año seleccionado.</Typography>
-            )}
+            <Paper elevation={3} sx={{ p: 2 }}>
+                <Typography variant="h4" gutterBottom>
+                    Cantidad total de productos ordenados por estado
+                </Typography>
+                <div>
+                    <label htmlFor="year">Seleccionar Estado:</label>
+                    <select id="year" value={selectedStatus} onChange={handleStatusChange} style={{ fontSize: '16px', marginLeft: '10px', padding: '2px' }}>
+                        <option value="OS">OS</option>
+                        <option value="A">A</option>
+                        <option value="IP">IP</option>
+                        {/* Add more years as needed */}
+                    </select>
+                </div>
+                <Typography variant="h6" gutterBottom>
+                    Cantidad: {data}
+                </Typography>
+            </Paper>
         </div>
     );
     
