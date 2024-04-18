6de4339c200c96e994fc5a28d2accc19184764d3
Alejandro Ortega <ort18248@uvg.edu.gt>
2024-04-18 17:21:33 -0600

    Update

--- backend/models/Relations/contains_model.py
+++
@@ -1,9 +0,0 @@
-from neomodel import StructuredRel, IntegerProperty, FloatProperty, RelationshipFrom
-
-
-class Contains(StructuredRel):
-    cantidad = IntegerProperty()
-    precio_unitario = FloatProperty()
-    subtotal = FloatProperty()
-
-    cantidad = RelationshipFrom('Inventory', 'Contains')
